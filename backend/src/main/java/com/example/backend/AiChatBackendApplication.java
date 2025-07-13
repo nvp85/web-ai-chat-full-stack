e835c5609e79d8cc07628ffda7d010793f07dd96
nvp85 <61716664+nvp85@users.noreply.github.com>
2025-07-12 20:16:36 -0500

    initial commit

---
+++ backend/src/main/java/com/example/backend/AiChatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiChatBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiChatBackendApplication.class, args);
+	}
+
+}

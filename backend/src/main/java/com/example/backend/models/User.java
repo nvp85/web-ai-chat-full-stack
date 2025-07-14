357789122bd686928d83e4cef1c6549e06a2f348
nvp85 <61716664+nvp85@users.noreply.github.com>
2025-07-13 23:47:01 -0500

    add a User constructor to initialize all fileds except for id

--- backend/src/main/java/com/example/backend/models/User.java
+++ backend/src/main/java/com/example/backend/models/User.java
@@ -18,4 +18,9 @@ public class User {
     private String password;
     private String email;
 
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
 }

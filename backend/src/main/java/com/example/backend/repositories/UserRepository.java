c59b339a5f476593ea5d8fba18a57d76f5e8c18e
nvp85 <61716664+nvp85@users.noreply.github.com>
2025-07-14 16:02:16 -0500

    add the findByEmail method to the user repository

--- backend/src/main/java/com/example/backend/repositories/UserRepository.java
+++ backend/src/main/java/com/example/backend/repositories/UserRepository.java
@@ -3,5 +3,8 @@ package com.example.backend.repositories;
 import com.example.backend.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
 }

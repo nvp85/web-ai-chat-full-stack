369c7dec7a23bf2afeea64d3a3a98b48791a8080
nvp85 <61716664+nvp85@users.noreply.github.com>
2025-07-14 00:48:28 -0500

    create the message repository

---
+++ backend/src/main/java/com/example/backend/repositories/MessageRepository.java
@@ -0,0 +1,7 @@
+package com.example.backend.repositories;
+
+import com.example.backend.models.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MessageRepository extends JpaRepository<Message, Integer> {
+}

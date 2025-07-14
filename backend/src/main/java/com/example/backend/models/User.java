1e6e6c8a78859472e6055f5848c20ba75ce63ca9
nvp85 <61716664+nvp85@users.noreply.github.com>
2025-07-13 22:50:04 -0500

    add no-arg constructor and change user's id type to int

--- backend/src/main/java/com/example/backend/models/User.java
+++ backend/src/main/java/com/example/backend/models/User.java
@@ -5,13 +5,15 @@ import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data // Lombok will generate getters, setters, toString, equals, and hashCode methods
+@NoArgsConstructor // Lombok will generate a no-args constructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+    private int id;
     private String username;
     private String password;
     private String email;

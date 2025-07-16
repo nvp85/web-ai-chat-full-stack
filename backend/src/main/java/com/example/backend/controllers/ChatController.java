76b6d3b190c5c8bf3d3ef69535e218a96346742d
nvp85 <61716664+nvp85@users.noreply.github.com>
2025-07-16 15:54:15 -0500

    add the create chat endpoint

--- backend/src/main/java/com/example/backend/controllers/ChatController.java
+++ backend/src/main/java/com/example/backend/controllers/ChatController.java
@@ -1,7 +1,11 @@
 package com.example.backend.controllers;
 
+import com.example.backend.DTOs.ChatCreationDTO;
 import com.example.backend.models.Chat;
+import com.example.backend.models.Message;
+import com.example.backend.models.User;
 import com.example.backend.services.ChatService;
+import com.example.backend.services.UserService;
 import eu.fraho.spring.securityJwt.base.dto.JwtUser;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -14,9 +18,11 @@ import java.util.List;
 @RequestMapping("/api/chats")
 public class ChatController {
     private final ChatService chatService;
+    private final UserService userService;
 
-    public ChatController(ChatService chatService) {
+    public ChatController(ChatService chatService, UserService userService) {
         this.chatService = chatService;
+        this.userService = userService;
     }
 
     @PreAuthorize("hasRole('ROLE_USER')")
@@ -24,6 +30,17 @@ public class ChatController {
     public List<Chat> getAllChats(@AuthenticationPrincipal JwtUser jwtUser) {
         // jwtUser username is the email of the user
         return chatService.getAllChats(jwtUser.getUsername());
+    }
+
+
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @PostMapping
+    public Message createChat(@AuthenticationPrincipal JwtUser jwtUser, @RequestBody ChatCreationDTO chatCreationDTO) {
+        // ChatCreationDTO contains the chat object and the first prompt
+        User user = userService.getUserByEmail(jwtUser.getUsername());
+        Chat chat = chatCreationDTO.getChat();
+        chat.setUser(user);
+        return chatService.createChat(chat, chatCreationDTO.getFirstPrompt());
     }
 
 }

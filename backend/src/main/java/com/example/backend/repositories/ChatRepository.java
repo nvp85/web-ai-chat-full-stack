5b08729459001aa60434927ed3665933020aeb92
nvp85 <61716664+nvp85@users.noreply.github.com>
2025-07-16 11:53:05 -0500

    change the id type in the chat repository to uuid

--- backend/src/main/java/com/example/backend/repositories/ChatRepository.java
+++ backend/src/main/java/com/example/backend/repositories/ChatRepository.java
@@ -4,7 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.backend.models.Chat;
 
 import java.util.List;
+import java.util.UUID;
 
-public interface ChatRepository extends JpaRepository<Chat, Integer> {
+public interface ChatRepository extends JpaRepository<Chat, UUID> {
     public List<Chat> findAllByUserEmail(String email); // Method to find all chats by user email
 }

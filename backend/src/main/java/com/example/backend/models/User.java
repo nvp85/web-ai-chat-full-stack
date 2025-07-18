9360291be65bc1520b5bde2b97b10234a1b24c1b
nvp85 <61716664+nvp85@users.noreply.github.com>
2025-07-18 15:38:39 -0500

    add chats to the User model

--- backend/src/main/java/com/example/backend/models/User.java
+++ backend/src/main/java/com/example/backend/models/User.java
@@ -5,6 +5,8 @@ import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Entity
 @Data // Lombok will generate getters, setters, toString, equals, and hashCode methods
 @NoArgsConstructor // Lombok will generate a no-args constructor
@@ -14,9 +16,13 @@ public class User {
     private int id;
     private String username;
     @JsonIgnore
-    private String password; // Password should be stored securely (hashed)
+    private String password; // Password is stored securely (hashed)
     @Column(unique=true)
     private String email;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    @OrderBy("lastModified DESC")
+    private List<Chat> chats;
 
     public User(String username, String password, String email) {
         this.username = username;

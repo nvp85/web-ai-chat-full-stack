5945a3c68ea1cd62b100622aee320db855cbfb95
nvp85 <61716664+nvp85@users.noreply.github.com>
2025-07-18 21:04:07 -0500

    add the generateTitle method to the google service

--- backend/src/main/java/com/example/backend/services/GoogleAiService.java
+++ backend/src/main/java/com/example/backend/services/GoogleAiService.java
@@ -1,7 +1,6 @@
 package com.example.backend.services;
 
 import com.example.backend.models.Message;
-import com.google.genai.Chat;
 import com.google.genai.Client;
 import com.google.genai.types.Content;
 import com.google.genai.types.GenerateContentConfig;
@@ -14,6 +13,7 @@ import java.util.List;
 public class GoogleAiService {
 
     private final Client client = new Client();
+    private final String model = "gemini-2.0-flash-001";
     //  gets the API key from the environment variable `GOOGLE_API_KEY`.
 
     public Message getResponse(List<Message> messages) {
@@ -30,12 +30,18 @@ public class GoogleAiService {
                 .build()
         ).toList();
 
-        Chat chat = client.chats.create("gemini-2.0-flash-001", config);
-        GenerateContentResponse response = chat.sendMessage(history);
+        GenerateContentResponse response = client.models.generateContent(model, history, config);
         return new Message(response.text(), "model");
     }
 
-
+    public String generateTitle(String firstPrompt) {
+        Content instruction = Content.fromParts(Part.fromText("Generate a concise title for the user's message"));
+        GenerateContentConfig config = GenerateContentConfig.builder()
+                .systemInstruction(instruction)
+                .build();
+        GenerateContentResponse response = client.models.generateContent(model, firstPrompt, config);
+        return response.text();
+    }
 }
 
 

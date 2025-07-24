3ad1592122dd2690a694186a386257afc50b860e
nvp85 <61716664+nvp85@users.noreply.github.com>
2025-07-23 20:56:55 -0500

    create the createUser function

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -38,21 +38,48 @@ export async function getAuthToken(creds) {
 const api_url = base_url + "/api";
 
 // fetch user's data
-export async function getUserData() {
-    const response = await fetch(api_url + "users",
+export async function getUserData(token) {
+    const response = await fetch(api_url + "/users/me",
         {
             method: "get",
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': localStorage.getItem('auth-token')
-            }   
+                'Authorization': token // localStorage.getItem('auth-token')
+            }
         }
     );
     const data = await response.json();
+    if (response.status === 401) {
+        throw new Error("Invalid token.")
+    }
     if (!response.ok) {
         throw new Error("Failed to fetch user data.");
     }
     return data;
 }
 
+export async function createUser(newUser) {
+    const response = await fetch(api_url + "/users",
+        {
+            method: "post",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newUser)
+        }
+    );
+    console.log(response);
+    if (!response.ok) {
+        throw new Error("Failed to register a new user.");
+    }
+}
 
+// getAuthToken({ username: "demouser@example.com", password: "qwerty" })
+//     .then(getUserData).then(console.log)
+
+// let testUser = {
+// 	name: "demoUser",
+// 	email: "test@example.co",
+// 	password: "qwertyD1*!vvv"
+// }
+// createUser(testUser)

820ac976258ea04dca8500bdd1585e06b60f33c4
nvp85 <61716664+nvp85@users.noreply.github.com>
2025-08-04 00:43:04 -0500

    add google api to the footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -3,7 +3,12 @@ import './Footer.css';
 export default function Footer() {
     return (
         <footer>
-            <p>Powered by <a href="https://platform.openai.com/docs/api-reference/chat" target="_blank">OpenAI API</a></p>
+            <p>Powered by <a href="https://platform.openai.com/docs/api-reference/chat" target="_blank"> 
+                    OpenAI API
+                </a> and <a href="https://ai.google.dev/gemini-api/docs/quickstart#java" target="_blank"> 
+                    Google AI API
+                </a>
+            </p>
         </footer>
     )
 }

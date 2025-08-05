df8956265ce7f314a9ec51adb8f35acbf32e9583
nvp85 <61716664+nvp85@users.noreply.github.com>
2025-08-04 19:48:22 -0500

    refactor API calls: extract the common part into a function

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,27 +1,44 @@
 
+// In this file there are functions for making API calls to the backend
+// that are used across the app
+
 // base url
 const base_url = "http://localhost:8080";
 
+// API request - fetch wrapper (throws common errors)
+async function APIrequest(url, method, body, token) {
+    const authHeaders = token ? { 'Authorization': token } : {};
+    const body = body ? { body: JSON.stringify(body) } : {};
+    const response = await fetch(url, {
+        method: method,
+        headers: {
+            'Content-Type': 'application/json',
+            ...authHeaders
+        },
+        ...body
+    });
+    if (response.status === 400) {
+        throw new Error("Bad request.");
+    }
+    if (response.status === 401) {
+        throw new Error("Invalid credentials.");
+    }
+    if (response.status === 404) {
+        throw new Error("Not found");
+    }
+    if (response.status === 503) {
+        throw new Error("API is currently unavailable, please try again later.");
+    }
+    return response;
+}
 
 // Auth
 
 // login user: get a token 
 // creds = {username, password}
 export async function getAuthToken(creds) {
-    const response = await fetch(base_url + "/auth/login",
-        {
-            method: "post",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(creds)
-        }
-    );
-    if (response.status === 401) {
-        throw new Error("Invalid credentials.");
-    }
+    const response = await APIrequest(base_url + "/auth/login", "post", creds, null);
     if (!response.ok) {
-        console.log(response.status)
         throw new Error("Something went wrong.");
     }
     const data = await response.json();
@@ -35,18 +52,7 @@ const api_url = base_url + "/api";
 // fetch user's data - initial user's data including the list of chats
 // gets fetched when the user/auth provider mounts 
 export async function getUserData(token) {
-    const response = await fetch(api_url + "/users/me",
-        {
-            method: "get",
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': token // localStorage.getItem('auth-token')
-            }
-        }
-    );
-    if (response.status === 401) {
-        throw new Error("Invalid token.")
-    }
+    const response = await APIrequest(api_url + "/users/me", "get", null, token);
     if (!response.ok) {
         throw new Error("Failed to fetch user data.");
     }
@@ -56,15 +62,7 @@ export async function getUserData(token) {
 
 // register a new user acc
 export async function createUser(newUser) {
-    const response = await fetch(api_url + "/users",
-        {
-            method: "post",
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(newUser)
-        }
-    );
+    const response = await APIrequest(api_url + "/users", "post", newUser, null);
     if (response.status === 409) {
         throw new Error("This email is already in use.");
     }
@@ -75,19 +73,7 @@ export async function createUser(newUser) {
 
 // update User profile
 export async function updateUserProfile(newProfile, token) {
-    const response = await fetch(api_url + "/users/me",
-        {
-            method: "put",
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': token
-            },
-            body: JSON.stringify(newProfile)
-        }
-    );
-    if (response.status === 401) {
-        throw new Error("Invalid token.");
-    }
+    const response = await APIrequest(api_url + "/users/me", "put", newProfile, token);
     if (response.status === 409) {
         throw new Error("This email is already in use.");
     }
@@ -98,19 +84,9 @@ export async function updateUserProfile(newProfile, token) {
 
 // Chat list
 
+// get user's chats
 export async function getChatList(token) {
-    const response = await fetch(api_url + "/chats",
-        {
-            method: "get",
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': token
-            }
-        }
-    );
-    if (response.status === 401) {
-        throw new Error("Invalid token.");
-    }
+    const response = await APIrequest(api_url + "/chats", "get", null, token);
     if (!response.ok) {
         throw new Error("Failed to fetch user's chat list.");
     }
@@ -121,112 +97,49 @@ export async function getChatList(token) {
 // Chat 
 
 export async function getChatById(chatId, token) {
-    const response = await fetch(api_url + "/chats/" + chatId,
-        {
-            method: "get",
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': token
-            }
-        }
-    );
-    const data = await response.json();
-    if (response.status === 401) {
-        throw new Error("Invalid token.")
-    }
+    const response = await APIrequest(api_url + "/chats/" + chatId, "get", null, token);
     if (!response.ok) {
         throw new Error("Failed to fetch the chat.");
     }
+    const data = await response.json();
     return data;
 }
 
-// accepts chat and its first prompt
+// Starts a new chat,
+// accepts chat and its first prompt in one obj
 export async function startChat(chatCreationObj, token) {
-    const response = await fetch(api_url + "/chats",
-        {
-            method: "post",
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': token
-            },
-            body: JSON.stringify(chatCreationObj)
-        }
-    );
-    const data = await response.json();
-    if (response.status === 401) {
-        throw new Error("Invalid token.");
-    }
-    if (response.status === 400) {
-        throw new Error("Bad request.");
-    }
+    const response = await APIrequest(api_url + "/chats", "post", chatCreationObj, token);
     if (response.status === 409) {
         throw new Error("The chat already exists.");
     }
     if (!response.ok) {
         throw new Error("Failed to create a new chat.");
     }
+    const data = await response.json();
     return data; // returns the new chat obj and a LLM's response
 }
 
 export async function updateChatTitle(chatId, newTitle, token) {
-    const response = await fetch(api_url + "/chats/" + chatId,
-        {
-            method: "put",
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': token
-            },
-            body: JSON.stringify({ title: newTitle })
-        }
-    );
-    const data = await response.json();
-    if (response.status === 401) {
-        throw new Error("Invalid token.");
-    } else if (response.status === 400) {
-        throw new Error("Bad request.");
-    } else if (!response.ok) {
+    const response = await APIrequest(api_url + "/chats/" + chatId, "put", { title: newTitle }, token);
+    if (!response.ok) {
         throw new Error("Failed to update the chat.");
     }
+    const data = await response.json();
     return data; // returns the updated chat obj
 }
 
 export async function deleteChat(chatId, token) {
-    const response = await fetch(api_url + "/chats/" + chatId,
-        {
-            method: "delete",
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': token
-            },
-        }
-    );
-    if (response.status === 401) {
-        throw new Error("Invalid token.");
-    } else if (response.status === 400) {
-        throw new Error("Bad request.");
-    } else if (!response.ok) {
-        throw new Error("Failed to update the chat.");
+    const response = await APIrequest(api_url + "/chats/" + chatId, "delete", null, token);
+    if (!response.ok) {
+        throw new Error("Failed to delete the chat.");
     }
 }
 
+// fetches chat messages by chat ID
 export async function getChatMessages(chatId, token) {
     const url = `${api_url}/chats/${chatId}/messages`;
-    const response = await fetch(url,
-        {
-            method: "get",
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': token
-            },
-        }
-    );
-    if (response.status === 401) {
-        throw new Error("Invalid token.");
-    } else if (response.status === 400) {
-        throw new Error("Bad request.");
-    } else if (response.status === 404) {
-        throw new Error("Not found");
-    } else if (!response.ok) {
+    const response = await APIrequest(url, "get", null, token);
+    if (!response.ok) {
         throw new Error("Failed to fetch the chat messages.");
     }
     const data = await response.json();
@@ -235,27 +148,15 @@ export async function getChatMessages(chatId, token) {
 
 // Messages
 
+// sends a message in a chat
 // the message is just a string here
 export default async function sendMessage(chatId, message, token) {
     const url = `${api_url}/chats/${chatId}/messages`;
-    const response = await fetch(url,
-        {
-            method: "post",
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': token
-            },
-            body: JSON.stringify({ content: message })
-        }
-    );
-    const data = await response.json();
-    if (response.status === 401) {
-        throw new Error("Invalid token.");
-    } else if (response.status === 400) {
-        throw new Error("Bad request.");
-    } else if (!response.ok) {
+    const response = await APIrequest(url, "post", { content: message }, token);
+    if (!response.ok) {
         throw new Error("Failed to send the message");
     }
+    const data = await response.json();
     return data; // returns the LLM's response
 }
 
@@ -274,35 +175,3 @@ export function getLLMs() {
     },
     ]
 }
-
-
-
-
-// let testUser = {
-// 	name: "demoUser",
-// 	email: "test@example.co",
-// 	password: "qwertyD1*!vvv"
-// }
-// createUser(testUser)
-
-//getChatById("83ee66e5-5650-41a1-bda6-660eaa2f1a8c", token).then(console.log);
-// getChatList(token).then(console.log);
-
-// let newChat = {
-// 	"chat": {
-// 	"id": "0eb9e3c4-47ba-48b3-8abd-55fbf43b9cba",
-// 	"llModel": {
-// 		"id": 2
-// 	}
-// 	},
-// 	"firstPrompt": "Describe the primary responsibilities of a Software Developer on a team."
-// }
-
-// startChat(newChat, token).then(console.log);
-// updateChatTitle("0eb9e3c4-47ba-48b3-8abd-55fbf43b9cba", "Responsibilities of a software dev", token).then(console.log);
-// deleteChat("76e4bda5-817c-4acf-9608-04f6d01feae6", token); 
-// getChatList(token).then(console.log); - there was a phantom read? I could see the chat that has just been deleted!
-// getChatMessages("0eb9e3c4-47ba-48b3-8abd-55fbf43b9cba", token).then(console.log);
-// sendMessage("0eb9e3c4-47ba-48b3-8abd-55fbf43b9cba", "What do designers do?", token).then(console.log);
-// getChatMessages("1f3afa62-7f29-4eee-b677-1fc8ec91406c", token).then(console.log);
-// sendMessage("1f3afa62-7f29-4eee-b677-1fc8ec91406c", "Tell about CBT.", token).then(console.log);

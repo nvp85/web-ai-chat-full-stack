28645a2683d916358fa613454a4e70eac69801fe
nvp85 <61716664+nvp85@users.noreply.github.com>
2025-08-04 23:27:23 -0500

    delete unused message repo

--- backend/src/main/java/com/example/backend/repositories/MessageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend.repositories;
-
-import com.example.backend.models.Message;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MessageRepository extends JpaRepository<Message, Integer> {
-}

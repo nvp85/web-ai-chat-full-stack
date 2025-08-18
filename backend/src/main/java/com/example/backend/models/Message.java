d79047092e42417895d3d997615d38d84e68fdb2
nvp85 <61716664+nvp85@users.noreply.github.com>
2025-08-17 23:51:35 -0500

    add custom analyzer

--- backend/src/main/java/com/example/backend/models/Message.java
+++ backend/src/main/java/com/example/backend/models/Message.java
@@ -23,20 +23,25 @@ public class Message {
     @JoinColumn(name = "chat_id", nullable = false)
     private Chat chat; // The chat to which this message belongs
 
-    @FullTextField
+    @FullTextField(analyzer = "english")
     @Column(columnDefinition = "TEXT")
     private String content; // The content of the message
     private String role; // The role of the message (e.g., "user", "assistant")
 
     @Transient
+    @JsonIgnore
     @IndexingDependency(derivedFrom = @ObjectPath(@PropertyValue(propertyName = "content")), extraction = @ContainerExtraction(extract = ContainerExtract.NO))
     @VectorField(dimension = 768, vectorSimilarity = VectorSimilarity.COSINE)
     private float[] embedding;
 
-    @KeywordField
+    @JsonIgnore
+    @GenericField
     @Transient
-    @IndexingDependency(derivedFrom = @ObjectPath(@PropertyValue(propertyName = "chat")))
-    private String ownerEmail;
+    @IndexingDependency(derivedFrom = @ObjectPath({
+            @PropertyValue(propertyName = "chat"),
+            @PropertyValue(propertyName = "user"),
+            @PropertyValue(propertyName = "id")}))
+    private int ownerId;
 
     public Message(String content, String role, Chat chat) {
         this.content = content;
@@ -47,5 +52,9 @@ public class Message {
     public Message(String content, String role) {
         this.content = content;
         this.role = role;
+    }
+
+    public int getOwnerId() {
+        return chat.getUser().getId();
     }
 }

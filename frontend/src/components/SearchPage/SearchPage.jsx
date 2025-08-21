2ed43416f8bddaa7281344b9dff1d2d5f3595f4c
nvp85 <61716664+nvp85@users.noreply.github.com>
2025-08-20 20:58:04 -0500

    add an error modal and a loading message

--- frontend/src/components/SearchPage/SearchPage.jsx
+++ frontend/src/components/SearchPage/SearchPage.jsx
@@ -3,21 +3,46 @@ import { searchMessages } from "../../api/api";
 import "./SearchPage.css";
 import { NavLink } from "react-router";
 import { useAuth } from "../../hooks/useAuth";
+import ErrorModal from "../Modal/ErrorModal";
+import { FaSpinner } from "react-icons/fa";
 
 
 export default function SearchPage() {
     const [searchResult, setSearchResult] = useState(null);
     const [query, setQuery] = useState("");
     const [searchType, setSearchType] = useState("text");
-    const {token} = useAuth();
+    const { token, handleUnauthorized } = useAuth();
+    const [error, setError] = useState(null);
+    const [searching, setSearching] = useState(false);
 
     async function search(e) {
         e.preventDefault();
+        setSearchResult(null);
         if (query.length < 3) {
+            setError("The query must be at least 3 characters long.");
             return;
         }
-        const messages = await searchMessages(query, searchType, token);
-        setSearchResult(messages);
+        try {
+            setSearching(true);
+            const messages = await searchMessages(query, searchType, token);
+            setSearchResult(messages);
+        } catch (err) {
+            if (err.message === "Invalid credentials.") {
+                handleUnauthorized();
+                return;
+            }
+            setError(err.message);
+        }
+        setSearching(false);
+    }
+    function displaySearchResults() {
+        if (searchResult?.length === 0) {
+            return (<p>Noting was found.</p>)
+        }
+        return searchResult.map(m => <div key={m.message.id} className="search-result-item">
+            <p>Chat: <NavLink to={`/chats/${m.chatId}`}>{m.chatTitle}</NavLink></p>
+            <p>{m.message.content}</p>
+        </div>)
     }
 
     return (
@@ -59,13 +84,12 @@ export default function SearchPage() {
                 <button className="btn">Search</button>
             </form>
             {
-                searchResult &&
-                searchResult.map(m => <div key={m.message.id} className="search-result-item">
-                    <p>Chat: <NavLink to={`/chats/${m.chatId}`}>{m.chatTitle}</NavLink></p>
-                    <p>{m.message.content}</p>
-                </div>)
+                searchResult !== null && displaySearchResults()                
             }
-
+            { searching && <p>Searching <FaSpinner className="spinner"/> </p>}
+            { error && 
+                <ErrorModal onClose={() => setError("")}> {error} </ErrorModal>
+            }
         </div>
     )
 }
